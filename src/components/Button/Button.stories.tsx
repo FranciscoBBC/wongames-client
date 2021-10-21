528f922cd62fce586849b8608030e20d1ab38282
franciscoBBC <francisco.ruf@gmail.com>
2021-10-21 10:21:08 -0300

    creating component Button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,29 @@
+import Button from '.'
+import { Story, Meta } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      type: ''
+    }
+  }
+} as Meta
+
+export const basic: Story = (args) => <Button {...args} />
+export const withIcon: Story = (args) => <Button {...args} />
+
+basic.args = {
+  children: 'basic'
+}
+
+withIcon.args = {
+  size: 'small',
+  children: 'button with icon',
+  icon: <AddShoppingCart />
+}
