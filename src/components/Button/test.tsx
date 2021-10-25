684f277892c0fe53cd6f6c3b36a3e7ab1a64d349
franciscoBBC <francisco.ruf@gmail.com>
2021-10-25 09:09:40 -0300

    refactoring Button to be a link

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -51,4 +51,17 @@ describe('<Button />', () => {
     expect(screen.getByText(/buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
+
+  it('should render button as a link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        buy now
+      </Button>
+    )
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
